ad8bfb1412095b20b1d0ad07943cab5ae92ccb70
Bryan <bryan@iosappdev.org>
2019-07-09 17:16:57 -0700

    NSUserDefault Practice

--- TodoApp/TodoListViewController.swift
+++ TodoApp/TodoListViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Study iOS", "Go Biking", "Eat"];
+  //  var itemArray = ["Study iOS", "Go Biking", "Eat"];
+    var itemArray = [String]();
+    let defaults = UserDefaults.standard;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items;
+        }
+        else {
+            print("No user defaults");
+            //itemArray = self.defaults.set([String], forKey: "TodoListArray");
+            
+        }
         // Do any additional setup after loading the view.
     }
+    
+
     
     //MARK - TableView Datasource Methods
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -34,13 +46,21 @@ class TodoListViewController: UITableViewController {
         //print(itemArray[indexPath.row]);
         tableView.deselectRow(at: indexPath, animated: true);
         
-        
         if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none;
         }
         else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark;
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (deleteAction, IndexPath) in
+            self.itemArray.remove(at: indexPath.row);
+            self.tableView.reloadData();
+        }
+        deleteAction.backgroundColor = UIColor.red;
+        return [deleteAction];
     }
     
     //MARK - Add New Items
@@ -51,6 +71,7 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todey Item", message: "", preferredStyle: .alert);
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!);
+            self.defaults.set(self.itemArray, forKey: "TodoListArray");
             self.tableView.reloadData();
         }
         alert.addTextField { (alertTextField) in
