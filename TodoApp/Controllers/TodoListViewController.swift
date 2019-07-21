5fb5681979d99ed88d36efc6000b71c2e59646af
Bryan <bryan@iosappdev.org>
2019-07-21 16:07:22 -0700

    NSUserDefault Version
    *Hit limitation of not being able to save a custom object into NSUserDefaults.

--- TodoApp/Controllers/TodoListViewController.swift
+++ TodoApp/Controllers/TodoListViewController.swift
@@ -11,19 +11,29 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
   //  var itemArray = ["Study iOS", "Go Biking", "Eat"];
-    var itemArray = [String]();
+    var itemArray = [Item]();
     let defaults = UserDefaults.standard;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        
+        let newItem = Item();
+        newItem.title = "Find Mike";
+        itemArray.append(newItem);
+        
+        let newItem2 = Item();
+        newItem2.title = "Study";
+        itemArray.append(newItem2);
+        
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items;
         }
-        else {
-            print("No user defaults");
-            //itemArray = self.defaults.set([String], forKey: "TodoListArray");
-            
-        }
+//        else {
+//            print("No user defaults");
+//            //itemArray = self.defaults.set([String], forKey: "TodoListArray");
+//
+//        }
         // Do any additional setup after loading the view.
     }
     
@@ -38,20 +48,28 @@ class TodoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath);
-        cell.textLabel?.text = itemArray[indexPath.row];
+        
+        let item = itemArray[indexPath.row];
+        
+        cell.textLabel?.text = item.title;
+        cell.accessoryType = (item.done) ? .checkmark : .none;
+
         return cell;
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row]);
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done;
+        tableView.reloadData();
         tableView.deselectRow(at: indexPath, animated: true);
         
-        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none;
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark;
-        }
+//        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none;
+//        }
+//        else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark;
+//        }
     }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
@@ -70,7 +88,13 @@ class TodoListViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Add New Todey Item", message: "", preferredStyle: .alert);
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!);
+            
+            
+            let newItem = Item();
+            newItem.title = textField.text!;
+            
+            
+            self.itemArray.append(newItem);
             self.defaults.set(self.itemArray, forKey: "TodoListArray");
             self.tableView.reloadData();
         }
---
+++ TodoApp/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  TodoApp
+//
+//  Created by Bryan Cuevas on 7/17/19.
+//  Copyright © 2019 BC88888. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title:String = "";
+    var done : Bool = false;
+}
